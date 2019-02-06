ee38388e16f35dbaa484d190a56ca57eb81b3383
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2019-02-06 17:14:27 +0900

    Make accountbook

---
+++ contracts/accountbook/accountbook.cpp
@@ -0,0 +1,62 @@
+#include <eosiolib/eosio.hpp>
+
+using namespace eosio;
+
+class [[eosio::contract]] accountbook : public eosio::contract {
+  public:
+    accountbook(
+      name receiver,
+      name code,
+      datastream<const char*> ds
+    ):contract(receiver, code, ds) {}
+
+    [[eosio::action]]
+    void upsert(
+      name user,
+      std::string name,
+      std::string mail,
+      std::string about
+    ) {
+      require_auth(user);
+      account_index accounts(_code, _code.value);
+      auto iterator = accounts.find(user.value);
+      if (iterator == accounts.end()) {
+        accounts.emplace(user, [&](auto& row) {
+          row.key = user;
+          row.name = name;
+          row.mail = mail;
+          row.about = about;
+        });
+      } else {
+        accounts.modify(iterator, user, [&](auto& row) {
+          row.key = user;
+          row.name = name;
+          row.mail = mail;
+          row.about = about;
+        });
+      }
+    }
+
+    [[eosio::action]]
+    void erase(name user) {
+      require_auth(user);
+      account_index accounts(_code, _code.value);
+      auto iterator = accounts.find(user.value);
+      eosio_assert(iterator != accounts.end(), "Record does not exist");
+      accounts.erase(iterator);
+    }
+
+  private:
+    struct [[eosio::table]] person {
+      name key;
+      std::string name;
+      std::string mail;
+      std::string about;
+
+      uint64_t primary_key() const { return key.value;}
+    };
+    typedef eosio::multi_index<"people"_n, person> account_index;
+};
+
+
+EOSIO_DISPATCH( accountbook, (upsert)(erase))
